4964aca9dfe1291e0c83462b2978bc35ad1ab58d
larltr <larltr17@gmail.com>
2023-11-23 17:21:51 +0600

    added annotation "@AndroidEntryPoint"

--- app/src/main/java/com/angelika/lesson_6android14/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/angelika/lesson_6android14/presentation/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.angelika.lesson_6android14
+package com.angelika.lesson_6android14.presentation.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.angelika.lesson_6android14.ui.theme.Lesson_6Android14Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
