1ea4e337d9f956eb4030bc9f1b04bcfa23da1cf5
Zununova <zununiazk@gmail.com>
2023-11-23 16:01:36 +0300

    finished writing fun fetchCharacters

--- app/src/main/java/com/angelika/lesson_6android14/data/remote/apiservice/HarryPotterApiService.kt
+++ app/src/main/java/com/angelika/lesson_6android14/data/remote/apiservice/HarryPotterApiService.kt
@@ -1,9 +1,10 @@
 package com.angelika.lesson_6android14.data.remote.apiservice
 
+import com.angelika.lesson_6android14.models.HarryPotterResponse
 import retrofit2.http.GET
 
 interface HarryPotterApiService {
 
     @GET("characters")
-    suspend fun fetchCharacters()
+    suspend fun fetchCharacters() : HarryPotterResponse
 }
